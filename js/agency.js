ad12353f017e7258dd6799b6a9b950bfe8aba8fb
Clément Bouillier <clement.bouillier@gmail.com>
2020-02-18 22:25:05 +0100

    Publishing to gh-pages (publish.sh)

---
+++ js/agency.js
@@ -0,0 +1,44 @@
+// Smooth scrolling via animate()
+$(document).ready(function(){
+
+  $("a").on('click', function(event) {
+    if (this.hash && window.location.pathname == "/") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // Navigation change on scroll
+  var maxOffset = 300;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= maxOffset) {
+      $('.navbar-default').addClass('navbar-shrink');
+    }
+    else {
+      $('.navbar-default').removeClass('navbar-shrink');
+    }
+  });
+
+  var maxOffset = 300;
+  if ($(window).scrollTop() >= maxOffset) {
+    $('.navbar-default').addClass('navbar-shrink');
+  }
+  else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
